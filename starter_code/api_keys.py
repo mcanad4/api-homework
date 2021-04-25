f1606101b13211dfb0c2049e81e9cefabb5ddfb1
Matt Canady <mcanad4@yahoo.com>
2021-04-24 20:55:37 -0400

    updated homework

--- starter_code/api_keys.py
+++ starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "c1cf1427fe2b8408e0dfadc9b2b2a60d"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyDEHvoGJBNqfKIWhCYoGayGQg0b3TBx3fc"
